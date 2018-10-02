a4434a0ee5ea78b1ab1d52e94c2ef56ece4ef16b
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    tucana: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -15,3 +15,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
