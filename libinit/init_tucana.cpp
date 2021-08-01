e20a2bc669026b3ff63151d0025f175553fb5fb9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-01 07:00:44 +0600

    tucana: {init -> libinit}: Adapt libinit for variants handling
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: Ife93ff1c06bac4b9a07b2ebee2b7b4a0a1fea3f0

---
+++ libinit/init_tucana.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_sm6150.h>
+
+static const variant_info_t tucana_global_info = {
+    .hwc_value = "GLOBAL",
+    .sku_value = "",
+
+    .brand = "Xiaomi",
+    .device = "tucana",
+    .marketname = "",
+    .model = "Mi Note 10",
+
+    .nfc = true,
+};
+
+static const variant_info_t tucana_cn_info = {
+    .hwc_value = "CN",
+    .sku_value = "",
+
+    .brand = "Xiaomi",
+    .device = "tucana",
+    .marketname = "",
+    .model = "MI CC9 Pro",
+
+    .nfc = true,
+};
+
+static const std::vector<variant_info_t> variants = {
+    tucana_global_info,
+    tucana_cn_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+    set_dalvik_heap();
+}
