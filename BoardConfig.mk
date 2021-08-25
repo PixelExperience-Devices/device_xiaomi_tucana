e8b80a91a891b5d97e3ca9e21ecc3c5d0c475b20
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-25 18:49:23 +0600

    tucana: libinit: Adapt to latest libinit changes
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I06f485be7215d0ff0b081cff270d2d2d3277fe36

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,8 +16,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_OTA_ASSERT_DEVICE := tucana
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_tucana
-TARGET_RECOVERY_DEVICE_MODULES := libinit_tucana
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_tucana
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_tucana
 
 # Audio
 TARGET_PROVIDES_AUDIO_EXTNS := true
